bd5b468a67c42fd5e9561df0da9f2781151a3657
Haseoo <dawid_szymkiewicz@hotmail.com>
2019-05-28 18:06:22 +0200

    Add app icon

--- src/main/java/com/rootnetsec/cryptofile/App.java
+++ src/main/java/com/rootnetsec/cryptofile/App.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 public class App extends Application {
 
@@ -19,6 +20,7 @@ public class App extends Application {
         Scene scene = new Scene(root);
 
         stage.setTitle("FXML Welcome");
+        stage.getIcons().add(new Image("/icon.png"));
         stage.setScene(scene);
         stage.show();
     }
